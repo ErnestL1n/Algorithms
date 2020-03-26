bf3362a3d98b1d186893bcfc380c243492c58d7d
ErnestL1n <52807284+ErnestL1n@users.noreply.github.com>
2020-03-26 15:18:21 +0800

    Update dp_leetcode_EditDistance.c

--- dp_leetcode_EditDistance.c
+++ dp_leetcode_EditDistance.c
@@ -1,9 +1,9 @@
 //Author:
-//https://github.com/ErnestL1n/DataStructures
+//https://github.com/ErnestL1n/AlogorithmsInC
 //Reference:
 //https://www.amazon.com/Introduction-Algorithms-3rd-MIT-Press/dp/0262033844
 //https://www.geeksforgeeks.org
-//https://www.amazon.com/Fundamentals-Data-Structures-Ellis-Horowitz/dp/0929306406
+//https://leetcode.com/
 //environment:
 //https://visualstudio.microsoft.com/zh-hant/vs/?rr=https%3A%2F%2Fwww.google.com%2F
 
