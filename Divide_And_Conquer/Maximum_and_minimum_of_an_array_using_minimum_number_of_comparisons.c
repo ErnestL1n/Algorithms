67e2b103d5b6b0d4f582d9865fc30ab9176ebb56
ErnestL1n <52807284+ErnestL1n@users.noreply.github.com>
2020-05-02 23:08:23 +0800

    Create Maximum_and_minimum_of_an_array_using_minimum_number_of_comparisons.c

---
+++ Divide_And_Conquer/Maximum_and_minimum_of_an_array_using_minimum_number_of_comparisons.c
@@ -0,0 +1,8 @@
+//Write a C function to return minimum and maximum in an array. You program should make minimum number of comparisons.
+
+//author:https://github.com/ErnestL1n
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
